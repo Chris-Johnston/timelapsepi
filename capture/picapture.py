c50c33d91d96a34e4af18e09608d241468c1e58f
Chris Johnston <chjohnston@protonmail.com>
2019-07-11 22:44:49 -0700

    prevent creating new cameras because of memory limitations

--- capture/picapture.py
+++ capture/picapture.py
@@ -25,24 +25,24 @@ class PiCapture(CaptureMethod):
         self.dimensions = (x, y)
         self.iso = config[ATTR_PI]['iso']
         self.delay = config[ATTR_PI]['delay']
+        self.c = picamera.PiCamera(resolution=self.dimensions)
 
     def get_name(self) -> str:
         return "picam"
 
     def capture_image(self, path: str):
         # TODO: proper logging
-        c = picamera.PiCamera(resolution=self.dimensions)
-        c.iso = self.iso
+        self.c.iso = self.iso
         time.sleep(self.delay)
         
         # fix settings so images are more consistent
         # https://picamera.readthedocs.io/en/release-1.13/recipes1.html#capturing-consistent-images
-        c.shutter_speed = c.exposure_speed
-        c.exposure_mode = 'off'
-        g = c.awb_gains
-        c.awb_mode = 'off'
-        c.awb_gains = g
-        c.led = False
+        self.c.shutter_speed = self.c.exposure_speed
+        self.c.exposure_mode = 'off'
+        g = self.c.awb_gains
+        self.c.awb_mode = 'off'
+        self.c.awb_gains = g
+        self.c.led = False
 
         # capture the image to the file
-        c.capture(path)
+        self.c.capture(path)
